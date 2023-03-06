e2ab75c20af66567d9337d9d8a03aa16d9344f48
mhaao@kth.se <mhaao@kth.se>
2023-03-06 21:30:59 +0100

    dbconfig update

--- db.js
+++ db.js
@@ -25,5 +25,5 @@
 //                 process.exit(1); // Exit with an error code
 //             });
 //     });
-const { db } = require('../dbconfig');
+const { db } = require('./dbconfig');
 module.exports = { db };
